44e9e1102b17030a6e6109584c76f84b6de0ed1c
aleksei <fedakov@yamdex.ru>
2023-03-23 20:21:37 +0300

    st56

--- socialnetwork/racts1/src/components/Users/UsersContainer.jsx
+++ socialnetwork/racts1/src/components/Users/UsersContainer.jsx
@@ -1,7 +1,40 @@
 import React from "react";
-import Users from "./Users";
 import {connect}  from "react-redux";
+import { useEffect } from 'react';
 import { followAC, unfollowAC, setUsersAC,setCurrentPageAC, setUsersTotalCountAC } from "../../redux/users-reducer";
+
+import axios from "axios";
+import Users from "./Users";
+
+const UsersAPIComponent = (props) => {
+useEffect(()=>{
+
+axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${props.currentPage}&count=${props.pageSize}`).then(response =>{
+    props.setUsers(response.data.items);  
+    props.setTotalUsersCount(response.data.totalCount);
+})
+    
+
+
+
+
+},[]) 
+
+function onPageChangrd(pageNumber) {
+    props.setCurrentPage(pageNumber);
+    axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${props.pageSize}`).then(response =>{
+    props.setUsers(response.data.items);  
+    props.setTotalUsersCount(response.data.totalCount);
+    })}
+return <Users totalUserCount={props.totalUserCount}
+pageSize={props.pageSize}
+currentPage={props.currentPage}
+onPageChangrd={onPageChangrd}
+users={props.users}
+follow={props.follow}
+unfollow={props.unfollow}
+/>
+}
 const mapStateToProps= (state) => {
     return {
         
@@ -33,5 +66,5 @@ const mapDipatchToProps = (dispatch) =>{
 
     }
 }
-const UsersContainer =connect(mapStateToProps, mapDipatchToProps)(Users)
+const UsersContainer =connect(mapStateToProps, mapDipatchToProps)(UsersAPIComponent)
 export default UsersContainer;
